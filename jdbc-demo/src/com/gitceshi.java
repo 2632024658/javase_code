508ca5062357d2ae009f20cd14de7519ee6261ee
2632024658 <112460284+2632024658@users.noreply.github.com>
2023-08-16 17:39:39 +0800

    新添加了一个类

---
+++ jdbc-demo/src/com/gitceshi.java
@@ -0,0 +1,15 @@
+package com;
+
+/**
+ * @className: gitceshi
+ * 当前项目名称  JDBC2023
+ * @description: TODO 类描述
+ * @author: Mr.Baldrick Liu
+ * @date: 2023/08/16 17:37
+ * @Company: Copyright© [日期] by [作者或个人]
+ **/
+public class gitceshi {
+    public static void main(String[] args) {
+        System.out.println("我是测试的");
+    }
+}
